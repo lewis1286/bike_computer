92d26cfb650b14da6ba8d762bcc226a96db354df
Lewis Guignard <lewisguignard@coraxcyber.com>
2016-10-07 22:02:10 -0700

    added sdl_videodriver

--- ui.py
+++ ui.py
@@ -19,6 +19,8 @@ button_2_dims = [MARGIN, 2 * MARGIN + BUTTON_HEIGHT,
 button_3_dims = [MARGIN, 3 * MARGIN + 2 * BUTTON_WIDTH,
                  BUTTON_HEIGHT, BUTTON_WIDTH]
 
+
+os.putenv('SDL_VIDEODRIVER', 'fbcon')
 os.putenv('SDL_FBDEV', '/dev/fb1')
 os.putenv('SDL_MOUSEDEV', 'TSLIB')
 os.putenv('SDL_MOUSEDEV', '/dev/input/touchscreen')
